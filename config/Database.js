662c27470f5b5e426bf34a79cc590197b1ce8e4b
abeaar <140606965+abeaar@users.noreply.github.com>
2025-02-28 11:31:31 +0700

    Delete config directory

--- config/Database.js
+++
@@ -1,8 +0,0 @@
-import{Sequelize} from 'sequelize';
-
-const dbContext = new Sequelize('noteapp','root','',{
-    host: 'localhost',
-    dialect: 'mysql'
-}); // Database name
-
-export default dbContext;
